8ea7ba06770cd5474056359758a0b333bcd39164
brian13062006 <consattibrian@gmail.com>
2025-03-24 14:14:40 +0000

    novo commit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,53 +1,80 @@
 #include <stdio.h>
 
-#define TAMANHO 10
-#define TAMANHO_NAVIO 3
+#define TOTAL_LINHAS 11
+#define TOTAL_COLUNAS 5
 
-int main() {
-    int tabuleiro[TAMANHO][TAMANHO];
-
-    // 1. Inicializa o tabuleiro com 0 (representando água)
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
+void inicializarTabuleiro(int tabuleiro[TOTAL_LINHAS][TOTAL_COLUNAS]) {
+    for (int i = 0; i < TOTAL_LINHAS; i++) {
+        for (int j = 0; j < TOTAL_COLUNAS; j++) {
             tabuleiro[i][j] = 0;
         }
     }
+}
 
-    // 2. Posiciona os navios
-    // Navio horizontal na linha 1, começando na coluna 0
-    for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[0][i] = 3;
+
+void Cone(int tabuleiro[TOTAL_LINHAS][TOTAL_COLUNAS]) {
+    tabuleiro[0][2] = 1;
+    
+    tabuleiro[1][1] = 1;
+    tabuleiro[1][2] = 1;
+    tabuleiro[1][3] = 1;
+    
+    tabuleiro[2][0] = 1;
+    tabuleiro[2][1] = 1;
+    tabuleiro[2][2] = 1;
+    tabuleiro[2][3] = 1;
+    tabuleiro[2][4] = 1;
+}
+
+
+void Cruz(int tabuleiro[TOTAL_LINHAS][TOTAL_COLUNAS]) {
+    tabuleiro[4][2] = 2;
+    
+    for (int j = 0; j < TOTAL_COLUNAS; j++) {
+        tabuleiro[5][j] = 2;
     }
+    
+    tabuleiro[6][2] = 2;
+}
 
-    // Navio vertical na coluna 9, começando na linha 1
-    for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[i + 1][9] = 3;
-    }
 
-    // Navio na diagonal principal (ex: tabuleiro[i][i])
-    for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[i + 1][i + 1] = 3;
-    }
+void Octaedro(int tabuleiro[TOTAL_LINHAS][TOTAL_COLUNAS]) {
+   
+    tabuleiro[8][2] = 3;
+    
+    tabuleiro[9][1] = 3;
+    tabuleiro[9][2] = 3;
+    tabuleiro[9][3] = 3;
+    
+    tabuleiro[10][2] = 3;
+}
 
-    // Navio na diagonal secundária (ex: tabuleiro[i][TAMANHO - 1 - i])
-    for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[i][TAMANHO - 1 - i] = 3;
-    }
-
-    // 3. Exibe o tabuleiro no console
+void Tabuleiro(int tabuleiro[TOTAL_LINHAS][TOTAL_COLUNAS]) {
     printf("   ");
-    for (int j = 0; j < TAMANHO; j++) {
-        printf("%d ", j + 1); // Adicionando espaço para alinhamento correto
+    for (int j = 0; j < TOTAL_COLUNAS; j++) {
+        printf(" %c", 'A' + j);
     }
     printf("\n");
-
-    for (int i = 0; i < TAMANHO; i++) {
-        printf("%2d ", i + 1); // Alinhando o número da linha
-        for (int j = 0; j < TAMANHO; j++) {
-            printf("%d ", tabuleiro[i][j]);
+    
+    
+    for (int i = 0; i < TOTAL_LINHAS; i++) {
+        printf("%2d:", i + 1);
+        for (int j = 0; j < TOTAL_COLUNAS; j++) {
+            printf(" %d", tabuleiro[i][j]);
         }
         printf("\n");
     }
+}
 
+int main() {
+    int tabuleiro[TOTAL_LINHAS][TOTAL_COLUNAS];
+    inicializarTabuleiro(tabuleiro);
+    
+
+    Cone(tabuleiro);      
+    Cruz(tabuleiro);      
+    Octaedro(tabuleiro);  
+    Tabuleiro(tabuleiro);
+    
     return 0;
 }
